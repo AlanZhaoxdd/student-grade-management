e0073210617c6add4fbe912583201f4a191f4a81
AlanZhao <alan.zhao@example.com>
2025-04-21 19:14:31 +0800

    feat(entity): 新增 Course 实体类，定义课程基本字段结构

---
+++ springboot/src/main/java/com/example/entity/Course.java
@@ -0,0 +1,58 @@
+package com.example.entity;
+
+public class Course {
+    private Integer id;
+    private Integer name;
+    private Integer no;
+    private Integer descri;
+    private Integer times;
+    private Integer teacher;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getName() {
+        return name;
+    }
+
+    public void setName(Integer name) {
+        this.name = name;
+    }
+
+    public Integer getNo() {
+        return no;
+    }
+
+    public void setNo(Integer no) {
+        this.no = no;
+    }
+
+    public Integer getDescri() {
+        return descri;
+    }
+
+    public void setDescri(Integer descri) {
+        this.descri = descri;
+    }
+
+    public Integer getTimes() {
+        return times;
+    }
+
+    public void setTimes(Integer times) {
+        this.times = times;
+    }
+
+    public Integer getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Integer teacher) {
+        this.teacher = teacher;
+    }
+}
