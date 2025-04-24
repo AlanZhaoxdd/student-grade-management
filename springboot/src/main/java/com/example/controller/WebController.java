1e75a32ab7daf767b6437108e3c2adb59b268b26
AlanZhao <alan.zhao@example.com>
2025-04-24 18:23:02 +0800

    feat(controller): 实现注册接口逻辑

--- springboot/src/main/java/com/example/controller/WebController.java
+++ springboot/src/main/java/com/example/controller/WebController.java
@@ -1,8 +1,13 @@
 package com.example.controller;
 
+import cn.hutool.core.util.ObjectUtil;
 import com.example.common.Result;
+import com.example.common.RoleEnum;
+import com.example.entity.Account;
 import com.example.entity.Admin;
+import com.example.entity.Student;
 import com.example.service.AdminService;
+import com.example.service.StudentService;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +22,9 @@ public class WebController {
     @Resource
     private AdminService adminService;
 
+    @Resource
+    private StudentService studentService;
+
     /**
      * 默认请求接口
      */
@@ -29,10 +37,28 @@ public class WebController {
      * 登录接口
      */
     @PostMapping("/login")
-    public Result login(@RequestBody Admin admin) {
-        Admin dbAdmin = adminService.login(admin);
-        return Result.success(dbAdmin);
+    public Result login(@RequestBody Account account) {
+        Account dbAccount;
+        if (RoleEnum.ADMIN.name().equals(account.getRole())){  //管理员登录
+            dbAccount = adminService.login(account);
+        } else if (RoleEnum.STUDENT.name().equals(account.getRole())) {  //学生登录
+            dbAccount = studentService.login(account);
+        } else{
+            return Result.error("角色错误");
+        }
+        return Result.success(dbAccount);
     }
 
+    /**
+     * 注册接口
+     */
+    @PostMapping("/register")
+    public Result register(@RequestBody Account account) {
+        if(ObjectUtil.isEmpty(account.getUsername()) || ObjectUtil.isEmpty(account.getPassword())) {
+            return Result.error("账号或密码未填");
+        }
+       studentService.register(account);
+        return Result.success();
+    }
 
 }
