faadbf3a4a5dee964fdaa370365e991f4e44b425
AlanZhao <alan.zhao@example.com>
2025-04-25 18:59:46 +0800

    feat(student-entity): 新增 Student 实体类，继承 Account 并扩展学生信息字段

---
+++ springboot/src/main/java/com/example/entity/Student.java
@@ -0,0 +1,94 @@
+package com.example.entity;
+
+public class Student extends Account{
+
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+    private String gender;
+
+    private String birth;
+
+    private String avatar;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
